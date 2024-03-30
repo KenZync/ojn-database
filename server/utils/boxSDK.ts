543ec23ceb784155592f6496da1f2392a14b314b
KenZync <kenzync@pm.me>
2024-03-31 04:58:42 +0700

    fix: typo

--- server/utils/boxSDK.ts
+++ server/utils/boxSDK.ts
@@ -3,7 +3,7 @@ import BoxSDK from 'box-node-sdk'
 export const boxClient = () => {
 	const sdk = new BoxSDK({
 		clientID: process.env.NUXT_BOX_CLIENT_ID || '',
-		clientSecret: process.env.NUXT_BOX_CLIENT_SCRET || '',
+		clientSecret: process.env.NUXT_BOX_CLIENT_SECRET || '',
 		appAuth: {
 			keyID: process.env.NUXT_BOX_JWT_KEY_ID || '',
 			privateKey: process.env.NUXT_BOX_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
